2d961bbc4474dce6e7585346a3cd8057a0b5f812
DancingAxolotl <5020980+DancingAxolotl@users.noreply.github.com>
2020-02-21 17:20:52 +0200

    Update new tab icons

--- components/brave_new_tab_ui/api/topSites/index.ts
+++ components/brave_new_tab_ui/api/topSites/index.ts
@@ -2,16 +2,8 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // you can obtain one at http://mozilla.org/MPL/2.0/.
-
+import { defaultTopSitesData } from '../../stories/default/data/topSites'
 export type TopSitesData = NewTab.Site[]
-
-const defaultTopSites = [
-  {url:"https://telosgreen.org", title:"Telos Coin"},
-  {url:"https://bitdorado.exchange", title:"Bitdorado exchange"},
-  {url:"https://governance.rocks", title:"governance.rocks"},
-  {url:"https://bitcoin-subsidium.org", title:"Bitcoin Subsidium"},
-  {url:"https://libra-ai.org", title:"Free Libra"}
-]
 
 /**
  * Obtains the top sites
@@ -19,10 +11,8 @@ const defaultTopSites = [
 export function getTopSites (): Promise<TopSitesData> {
   return new Promise(resolve => {
     chrome.topSites.get((topSites: NewTab.Site[]) => {
-      if (topSites.length < defaultTopSites.length) {
-        topSites.push(defaultTopSites.slice(0, defaultTopSites.length - topSites.length))
-      }
-      resolve(topSites)
+      topSites.push(defaultTopSitesData.slice(0, max(defaultTopSitesData.length - topSites.length, 0)) as NewTab.Site[])
+      resolve(topSites || [])
     })
   })
 }
--- components/brave_new_tab_ui/stories/default/data/topSites.ts
+++ components/brave_new_tab_ui/stories/default/data/topSites.ts
@@ -2,48 +2,41 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import AppStoreFavicon from './siteIcons/appstore.png'
-import BraveFavicon from './siteIcons/brave.png'
-import FacebookFavicon from './siteIcons/facebook.png'
-import PlayStoreFavicon from './siteIcons/playstore.png'
-import TwitterFavicon from './siteIcons/twitter.png'
-import YouTubeFavicon from './siteIcons/youtube.png'
-
-export const defaultTopSitesData = [
-  {
-    name: 'App Store',
-    url: 'https://itunes.apple.com/app/brave-browser-fast-adblocker/id1052879175?mt=8',
-    favicon: AppStoreFavicon,
-    background: 'rgba(255,255,255,0.8)'
-  },
-  {
-    name: 'Brave Software',
-    url: 'https://brave.com',
-    favicon: BraveFavicon,
-    background: 'rgba(255,255,255,0.8)'
-  },
-  {
-    name: 'Facebook',
-    url: 'https://www.facebook.com/BraveSoftware/',
-    favicon: FacebookFavicon,
-    background: 'rgba(255,255,255,0.8)'
-  },
-  {
-    name: 'Play Store',
-    url: 'https://play.google.com/store/apps/details?id=com.brave.browser&hl=en_US',
-    favicon: PlayStoreFavicon,
-    background: 'rgba(255,255,255,0.8)'
-  },
-  {
-    name: 'Twitter',
-    url: 'https://twitter.com/brave',
-    favicon: TwitterFavicon,
-    background: 'rgba(255,255,255,0.8)'
-  },
-  {
-    name: 'YouTube',
-    url: 'https://www.youtube.com/bravesoftware',
-    favicon: YouTubeFavicon,
-    background: 'rgba(255,255,255,0.8)'
-  }
-]
+ import TelosCoinFavicon from './siteIcons/005.png'
+ import BitdoradoFavicon from './siteIcons/001.png'
+ import GovernanceFavicon from './siteIcons/000.png'
+ import SubsidiumFavicon from './siteIcons/004.png'
+ import LibraFavicon from './siteIcons/003.png'
+ 
+ export const defaultTopSitesData = [
+   {
+     name: 'Telos Coin',
+     url: 'https://telosgreen.org',
+     favicon: TelosCoinFavicon,
+     background: 'rgba(255,255,255,0.8)'
+   },
+   {
+     name: 'Bitdorado exchange',
+     url: 'https://bitdorado.exchange',
+     favicon: BitdoradoFavicon,
+     background: 'rgba(255,255,255,0.8)'
+   },
+   {
+     name: 'governance.rocks',
+     url: 'https://governance.rocks',
+     favicon: GovernanceFavicon,
+     background: 'rgba(255,255,255,0.8)'
+   },
+   {
+     name: 'Bitcoin Subsidium',
+     url: 'https://play.google.com/store/apps/details?id=com.brave.browser&hl=en_US',
+     favicon: SubsidiumFavicon,
+     background: 'rgba(255,255,255,0.8)'
+   },
+   {
+     name: 'Free Libra',
+     url: 'https://libra-ai.org',
+     favicon: LibraFavicon,
+     background: 'rgba(255,255,255,0.8)'
+   }
+ ]
