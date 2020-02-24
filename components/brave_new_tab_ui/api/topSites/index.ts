5c370b287692557502355e2f91d2ab138431df93
DancingAxolotl <5020980+DancingAxolotl@users.noreply.github.com>
2020-02-24 15:36:07 +0200

    For previous

--- components/brave_new_tab_ui/api/topSites/index.ts
+++ components/brave_new_tab_ui/api/topSites/index.ts
@@ -3,7 +3,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // you can obtain one at http://mozilla.org/MPL/2.0/.
 import { defaultTopSitesData } from '../../stories/default/data/topSites'
-import { NetworkInterfaceBase } from 'os'
 export type TopSitesData = NewTab.Site[]
 
 /**
